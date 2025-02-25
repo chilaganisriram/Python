eeba64007140de5ea3127f7835405812c02f574b
chilaganisriram <chilaganisriram07@gmail.com>
2025-02-25 12:53:54 +0530

    24-02-25

--- prac.py
+++ prac.py
@@ -1,4 +1,4 @@
-##leap year
+#leap year
 
 def Isaleapyear(n):
     if (n%4==0 and n%100!=0) or (n%400==0):
@@ -7,3 +7,22 @@ def Isaleapyear(n):
         return  "Not a leap year"
 
 print(Isaleapyear(1900))
+
+#24-02-25
+
+N=int(input("enter the number"))
+for i in range(21):
+    print(N,"*",i,"=",N*i)
+    
+#2
+# num=int(input())
+fac=1
+while num!=1:
+    fac=fac*num
+    num=num-1
+print(fac)
+
+#3
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+        print(i)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+N=int(input("enter the number"))
+for i in range(21):
+    print(N ,"*" ,i ,"=", N*i)
